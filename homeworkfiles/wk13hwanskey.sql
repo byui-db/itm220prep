ebe23292fe4b7ace447d7acc3652b06c742c697b
NicholasBoss <NicholasBoss0914@gmail.com>
2025-03-20 09:37:17 -0600

    added dense_rank question to week 12 workbook. Created week 13 hw and answer key

---
+++ homeworkfiles/wk13hwanskey.sql
@@ -0,0 +1,31 @@
+-- Background:
+-- You have been hired by BYU-I Air to help sort through the airportdb database. 
+-- Each week you will receive a file from your manager with questions that 
+-- need answered by writing queries against the database. 
+-- This week your manager wants you to understand what views and CTE expressions are. 
+
+USE airportdb;
+
+-- ---------------------------------------------------------------------------------
+-- 1. Who is the top 10 airlines by customer activity? (number of customers served)
+
+-- ---------------------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------------------------------------------
+-- 2. Who are the top 10 airlines by revenue?
+
+-- --------------------------------------------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------------------------------------------
+-- 3. Which airlines are on both lists?
+
+-- --------------------------------------------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------------------------------------------
+-- 4. Compare the top 10 airlines by revenue vs the 11 – 20 airlines by revenue. How big of a gap is there?
+
+-- --------------------------------------------------------------------------------------------------------
+
